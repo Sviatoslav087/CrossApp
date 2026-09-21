0a6ff49cb9100dc2867cf3a10a2e386bc0008ee6
Sviatoslav087 <Sviatoslav.Levchuk@lnu.edu.ua>
2026-09-21 21:38:15 +0300

    lab02

--- src/Cli/Program.cs
+++ src/Cli/Program.cs
@@ -1,6 +1,6 @@
 ﻿using Core;
  EnvironmentReport report = EnvironmentInfo.Collect();
- Console.WriteLine("CrossApp – інформація про середовище");
+ Console.WriteLine("CrossApp – Windows");
  Console.WriteLine(new string('-', 52));
  Console.WriteLine($"ОС : {report.OsDescription}");
  Console.WriteLine($"Runtime : {report.FrameworkDescription}");
@@ -8,3 +8,4 @@
  Console.WriteLine($"RID (визначено): {report.DetectedRid}");
  Console.WriteLine($"RID (від .NET) : {report.ReportedRid}");
  Console.WriteLine($"Каталог : {report.BaseDirectory}");
+
